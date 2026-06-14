39ee7fd809d17ce8d459e03fe8ff4e41426246ab
petsereypanha <sereypanhapet@gmail.com>
2026-06-14 19:36:30 +0700

    first commit

---
+++ basic/Basic_DataType.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+  int max_recursive_calls = 100;
+  char io_mode = 'w';
+  float throttle_speed = 0.2;
+
+  // don't touch below this line
+  printf("Max recursive calls: %d\n", max_recursive_calls);
+  printf("IO mode: %c\n", io_mode);
+  printf("Throttle speed: %f\n", throttle_speed);
+
+ char *will_never_hear_again =
+      "Hey TJ, when is the memory course in C gonna be done?";
+
+  // don't touch below this line
+  printf("%s\n", will_never_hear_again);
+  return 0;
+
+  return 0;
+}
---
+++ basic/Is_Compile.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+  printf("starting sneklang tools\n");
+  printf("finished sneklang tools\n");
+  return 0;
+}
