ed978aeb108f1b5d1a0b148662c81db07697f490
petsereypanha <sereypanhapet@gmail.com>
2026-06-20 00:20:39 +0700

    Scaling Coordinate

---
+++ structs/Scaling_Coordinate.c
@@ -0,0 +1,15 @@
+#include "coord.h"
+
+struct Coordinate new_coord(int x, int y, int z) {
+  struct Coordinate coord = {.x = x, .y = y, .z = z};
+  return coord;
+}
+
+struct Coordinate scale_coordinate(struct Coordinate coord, int factor) {
+  struct Coordinate scaled = {
+      .x = coord.x * factor,
+      .y = coord.y * factor,
+      .z = coord.z * factor
+    };
+  return scaled;
+}
---
+++ structs/Scaling_Coordinate.h
@@ -0,0 +1,11 @@
+#pragma once
+
+struct Coordinate {
+  int x;
+  int y;
+  int z;
+};
+
+struct Coordinate new_coord(int x, int y, int z);
+
+struct Coordinate scale_coordinate(struct Coordinate coord, int factor);
