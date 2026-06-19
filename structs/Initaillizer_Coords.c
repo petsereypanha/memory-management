b267d430950fc4764094063f1e257bc1acba9c71
petsereypanha <sereypanhapet@gmail.com>
2026-06-19 23:13:53 +0700

    Initializer and Struct

---
+++ structs/Initaillizer_Coords.c
@@ -0,0 +1,5 @@
+#include "coord.h"
+
+struct Coordinate new_coord(int x, int y, int z) {
+  return (Coordinate){.name = name, .lat = lat, .lon = lon};
+}
---
+++ structs/Initaillizer_Coords.h
@@ -0,0 +1,9 @@
+#pragma once
+
+struct Coordinate {
+  int x;
+  int y;
+  int z;
+};
+
+struct Coordinate new_coord(int x, int y, int z);
