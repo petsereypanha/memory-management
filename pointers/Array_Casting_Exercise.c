2f508d0f79f43b86c664ee7e26967d1e97e72943
petsereypanha <sereypanhapet@gmail.com>
2026-06-22 23:34:13 +0700

    Array Casting With Pointer

---
+++ pointers/Array_Casting_Exercise.c
@@ -0,0 +1,9 @@
+#include "exercise.h"
+#include <stdio.h>
+
+void dump_graphics(graphics_t gsettings[10]) {
+  int *ptr = (int *)gsettings;
+  for (int i = 0; i < 30; i++) {
+    printf("settings[%d] = %d\n", i, ptr[i]);
+  }
+}
