92223246d4c734ee24243273a3e11461745b2156
petsereypanha <sereypanhapet@gmail.com>
2026-06-26 23:47:43 +0700

    Helper Fields Union

--- union/Helper_Fields.h
+++ union/Helper_Fields.h
@@ -2,10 +2,9 @@
 
 typedef union PacketHeader {
   struct {
-    uint16_t src_port;   
-    uint16_t dest_port; 
-    uint32_t seq_num;    
-  } tcp_header;        
-
-  uint8_t raw;       
+    uint16_t src_port;
+    uint16_t dest_port;
+    uint32_t seq_num;
+  } tcp_header;
+  uint8_t raw[8];
 } packet_header_t;
